20541e1fef82da7d8a357e26a632cf04b439cc32
Yasmim Lazarim <yasslazarim@gmail.com>
2025-09-09 16:32:03 -0300

    feat: add Model and User classes with basic structure and configuration

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once(dirname(__FILE__, 2) . '/src/config/database.php');
-
-Database::getConnection();
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
