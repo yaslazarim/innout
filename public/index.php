6c11a8ddcff060d5b45c4ed631a55179de09dd29
Yasmim Lazarim <yasslazarim@gmail.com>
2025-09-11 16:05:34 -0300

    feat: implement dynamic filtering in Model's getSelect method

--- public/index.php
+++ public/index.php
@@ -3,3 +3,9 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
+$user = new User(['name' => 'Lucas', 'email' => 'lucas@email.com']);
+
+
+echo User::getSelect(['id' => '1'], 'name, email');
+echo '<br>';
+echo User::getSelect(['name' => 'Yasmim', 'email' => 'chaves@cod3r.com.br']);
