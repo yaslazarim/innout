6f3d1f9adcecbd762e8e8f59becf251ef731ef69
Yasmim Lazarim <yasslazarim@gmail.com>
2025-09-12 15:20:00 -0300

    feat: add login view and styles for user authentication

--- public/index.php
+++ public/index.php
@@ -1,13 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@email.com']);
-
-
-print_r(User::get(['name' => 'Yasmim'], 'id, name, email'));
-
-echo '<br>';
-
-print_r(User::get());
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
---
+++ src/views/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>In 'n Out</title>
+</head>
+
+<body>
+    <form class="form-login" action="#" method="">
+        <div class="login-card card">
+            <div class="card-header">
+                <i class="icofont-travelling mr-2"></i>
+                <span class="font-weight-light">In</span>
+                <span class="font-weight-bold mr-x">'n</span>
+                <span class="font-weight-light">Out</span>
+                <i class="icofont-runner-alt-1 ml-2"></i>
+
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input class="form-control"
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Digite seu email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input class="form-control"
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Digite sua senha">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-primary">Entrar</button>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
